253991734dc6f362fdbb75bbdfdc88f6f13b15b0
royren <360992830@qq.com>
2016-09-20 10:10:23 +1000

    ajax testing

--- src/main/java/com/roy/royspace/controller/IndexController.java
+++ src/main/java/com/roy/royspace/controller/IndexController.java
@@ -1,8 +1,12 @@
 package com.roy.royspace.controller;
 
+import com.roy.royspace.model.ContactForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by royren on 16/9/3.
@@ -15,7 +19,14 @@ public class IndexController {
      * @return
      */
     @RequestMapping("/")
-    public String index(Model model) {
+    public String index(ContactForm contactForm, Model model) {
         return "index";
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/thankyou",method = RequestMethod.POST)
+    public String thankyou(){
+        System.out.println("hihihi");
+        return null;
     }
 }
