40636fb5346465a5185381cd64ab4fde7db1fbb7
royren <360992830@qq.com>
2016-09-21 09:28:43 +1000

    add file

--- src/main/resources/META-INF/resources/js/contact_me.js
+++ src/main/resources/META-INF/resources/js/contact_me.js
@@ -21,7 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                //url: "././mail/contact_me.php",
+                url: "/thankyou",
                 type: "POST",
                 data: {
                     name: name,
